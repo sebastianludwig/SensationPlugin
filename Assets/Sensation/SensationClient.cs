74bb278b443024ac0c952a2c1a7248e02cfb57e9
Sebastian Ludwig <sebastian@lurado.de>
2014-05-25 16:36:23 +0200

    Prepared SensationClient for better error handling

--- Assets/Sensation/SensationClient.cs
+++ Assets/Sensation/SensationClient.cs
@@ -14,12 +14,11 @@ public class SensationClient {
 	
 	private bool shouldStopTransmitting = false;
 	private readonly object shouldStopTransmittingLock = new object();
-
 	
 	public static readonly SensationClient Instance = new SensationClient();
 
 	// Explicit static constructor to tell C# compiler
-	// not to mark type as beforefieldinit
+	// not to mark type as beforefieldinit (see http://csharpindepth.com/articles/general/singleton.aspx)
 	static SensationClient() {}
 
 	public SensationClient() {
@@ -49,10 +48,9 @@ public class SensationClient {
 		signal.Set();
 	}
 	
-	void Transmit(object serverName) {
+	#region background thread
+	private void Transmit(object serverName) {
 		string serverNameString = (string)serverName;
-		TcpClient client;
-		NetworkStream networkStream;
 		
 		try {
         	IPAddress[] serverIps = Dns.GetHostEntry(serverNameString).AddressList;
@@ -61,47 +59,59 @@ public class SensationClient {
 				return;
 			}
 			
-			client = new TcpClient();
-			client.Connect(new IPEndPoint(serverIps[0], 10000));
-			networkStream = client.GetStream();
+			ProcessingLoop(new IPEndPoint(serverIps[0], 10000));
     	} catch(Exception e) {
+			// TODO finer exception handling
+			// GetHostEntry
+			// whatever ProcessingLoop throws 
+			//		TcpClient.Connect() (probably implicitly called)
+			//		TcpClient.GetStream()
+			//		own exceptions if !connected or !canWrite
+			//		Serializer.SerializeWithLengthPrefix()?
 			Debug.LogError("Unable to connect to server " + serverNameString);
         	Debug.LogError(e);
 			return;
     	}
-		
-		try {
-			while (true) {
-				lock (shouldStopTransmittingLock) {
-					if (shouldStopTransmitting) {
+	}
+	
+	private void ProcessingLoop(IPEndPoint server) {
+		using (TcpClient client = new TcpClient(server))
+		using (NetworkStream networkStream = client.GetStream())
+		{
+			try {
+				while (true) {
+					lock (shouldStopTransmittingLock) {
+						if (shouldStopTransmitting) {
+							break;
+						}
+					}
+					
+					if (!client.Connected) {
+						Debug.LogError("Unable to send command - client disconnected");
 						break;
 					}
-				}
-				
-				if (!client.Connected) {
-					Debug.LogError("Unable to send command - client disconnected");
-					break;
-				}
-		
-				if (!networkStream.CanWrite) {
-					Debug.LogError("Can't write to network stream");
-					break;
-				}
-				
-				while (sensationQueue.Count > 0) {
-					Sensation sensation;
-					bool sensationDequeued = sensationQueue.TryDequeue(out sensation);
-					if (!sensationDequeued || sensation == null) {
+			
+					if (!networkStream.CanWrite) {
+						Debug.LogError("Can't write to network stream");
 						break;
 					}
-					Serializer.SerializeWithLengthPrefix(networkStream, sensation, PrefixStyle.Fixed32BigEndian);
+					
+					while (sensationQueue.Count > 0) {
+						Sensation sensation;
+						bool sensationDequeued = sensationQueue.TryDequeue(out sensation);
+						if (!sensationDequeued || sensation == null) {
+							break;
+						}
+						Serializer.SerializeWithLengthPrefix(networkStream, sensation, PrefixStyle.Fixed32BigEndian);
+					}
+					
+					signal.WaitOne();
 				}
-				
-				signal.WaitOne();
+			} finally {
+				networkStream.Close();
+				client.Close();
 			}
-		} finally {
-			networkStream.Close();
-			client.Close();
 		}
 	}
+	#endregion
 }
