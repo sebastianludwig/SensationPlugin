a6373ad916e58a75672de3bf1b2c0c1bec701d8a
Sebastian Ludwig <sebastian@lurado.de>
2014-08-06 20:25:23 +0200

    Updated scene to use new pattern features by example.

---
+++ Assets/DemoScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoScript : MonoBehaviour {
+	public TextAsset what;
+
+	// Use this for initialization
+	void Start () {
+		var hub = GameObject.FindWithTag("SensationHub");
+		hub.GetComponent<SensationHub>().LoadPattern(what);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
